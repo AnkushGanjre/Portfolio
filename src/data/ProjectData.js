1f410e1b67971ecda32c83a891ab7f1d122ce781
Ankush MWFT <149873707+AnkushMWFT@users.noreply.github.com>
2024-02-02 12:22:34 +0530

    Update

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -4,7 +4,7 @@ export const ProjectList = [
     img: "/WarzoneTactics.png",
     title: "Warzone Tactics: Duel of Bunkers",
     description:
-      "HI.",
+      "A multiplayer showdown where players compete to decode their opponent's strategy and obliterate their bunkers in strategic combat.",
     tech_stack: ["Unity", "Photon Fusion", "Blender", "Substance Painter"],
     github_url: "https://github.com/AnkushGanjre/WarzoneTactics",
     getapk_url: "https://drive.google.com/drive/folders/1H-93ID8M0lKGPMZP7MFPj4CQSmDC9Zmr",
@@ -13,7 +13,7 @@ export const ProjectList = [
     img: "/TicTacToe.png",
     title: "Multiplayer Tic Tac Toe",
     description:
-      "A safety application for the Renewable Resources Dept. of BCIT",
+      "Challenge friends or foes in this classic multiplayer showdown. Strategize, block, and outwit your opponent to claim victory in the ultimate game of X's and O's.",
     tech_stack: ["Unity 3D", "Photon Fusion 2", "Blender",],
     github_url: "https://github.com/AnkushGanjre/TicTacToe",
     getapk_url: "https://drive.google.com/drive/folders/1H-93ID8M0lKGPMZP7MFPj4CQSmDC9Zmr",
@@ -22,7 +22,7 @@ export const ProjectList = [
     img: "/Hyperhex.png",
     title: "HyperHex",
     description:
-      "An app where users can purchase online therapy sessions and chat with their therapists.",
+      "Dodge, weave, and collect power-ups as you strive to evade the encroaching Hex and survive the ultimate test of agility and reflexes.",
     tech_stack: ["Unity 3D", "Adobe Illustrator"],
     github_url: "https://github.com/AnkushGanjre/Hyperhex",
     getapk_url: "https://drive.google.com/drive/folders/1H-93ID8M0lKGPMZP7MFPj4CQSmDC9Zmr",
@@ -32,7 +32,7 @@ export const ProjectList = [
     img: "/SpikyEscape.png",
     title: "Spiky Escape",
     description:
-      "A pokedex app that displays pokemon data, plays OSTs and makes pokemons dance.",
+      "Survive the chaotic onslaught while dodging deadly spikes and staying one step ahead of disappearing ground.",
     tech_stack: ["Unity 3D", "Adobe Illustrator"],
     github_url: "https://github.com/AnkushGanjre/Spiky_Escape",
     getapk_url: "https://drive.google.com/drive/folders/1H-93ID8M0lKGPMZP7MFPj4CQSmDC9Zmr",
@@ -41,7 +41,7 @@ export const ProjectList = [
     img: "/MrPresident.png",
     title: "Mr. President",
     description:
-      "An app to detect if a job posting is a scam or not using its description.",
+      "Rise to power in this idle game by building your empire, upgrading your lifestyle, and navigating the world of politics on your journey to the presidency.",
     tech_stack: ["Unity 3D", "Adobe Illustrator"],
     github_url: "https://github.com/AnkushGanjre/Mr_President",
     getapk_url: "https://drive.google.com/drive/folders/1H-93ID8M0lKGPMZP7MFPj4CQSmDC9Zmr",
@@ -50,18 +50,9 @@ export const ProjectList = [
     img: "/KinderQuest.png",
     title: "Kinder Quest",
     description:
-      "An app to detect if a job posting is a scam or not using its description.",
+      "An interactive app designed to kickstart children's learning journey, explore in a fun and engaging environment, fostering early development and skills.",
     tech_stack: ["Unity 3D", "Adobe Illustrator"],
     github_url: "https://github.com/AnkushGanjre/Kinder_Quest",
-    getapk_url: "https://drive.google.com/drive/folders/1H-93ID8M0lKGPMZP7MFPj4CQSmDC9Zmr",
-  },
-  {
-    img: "/LegacyCarrom.png",
-    title: "Legacy Carrom",
-    description:
-      "An app to detect if a job posting is a scam or not using its description.",
-    tech_stack: ["Unity 3D", "Adobe Illustrator"],
-    github_url: "https://github.com/AnkushGanjre/Legacy_carom",
     getapk_url: "https://drive.google.com/drive/folders/1H-93ID8M0lKGPMZP7MFPj4CQSmDC9Zmr",
   },
 ];
